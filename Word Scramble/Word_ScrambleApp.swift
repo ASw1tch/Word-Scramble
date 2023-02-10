9cd231da1f2ec571641a238ea27ee2fbaa21c7fe
Anatolii Petrov <hello@aswitch.me>
2023-02-10 22:18:12 +0100

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Anatoliy Petrov on 10.2.23..
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
