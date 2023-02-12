47dc004ffba4232a5ed11d6a5b6445cb18b11fb0
Anatolii Petrov <hello@aswitch.me>
2023-02-12 22:43:36 +0100

    Full game with icons

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -7,11 +7,30 @@
 
 import SwiftUI
 
+let MyBackGroundColor = LinearGradient(stops: [
+    .init(color: Color(red: 229/255, green: 227/255, blue: 201/255), location: 0.3),
+    .init(color: Color(red: 180/255, green: 207/255, blue: 176/255), location: 0.6),
+    .init(color: Color(red: 148/255, green: 180/255, blue: 159/255), location: 1)
+], startPoint: .bottom, endPoint: .top)
+
 struct ContentView: View {
     
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    init() {
+        //Appears to be the key to making Navigation View transparent
+        UITableView.appearance().backgroundColor = .clear
+        //Just for reference, not sure if opacity can be adjusted
+        UINavigationBar.appearance().barTintColor = .clear
+    }
+
+    
     
     var body: some View {
         NavigationView {
@@ -30,21 +49,82 @@ struct ContentView: View {
                         
                     }
                 }
+                
             }
+            
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
+            .background(MyBackGroundColor.ignoresSafeArea())
         }
+        .foregroundColor(.green)
+        
+        
     }
+        
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else {return}
         
-        //Extra validation to come
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used alrady", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         withAnimation{
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+    }
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "locker"
+                return
+            }
+        }
+        fatalError("Couldn't load start.txt file.")
+    }
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
